6415b3f86e0266a2c98073cb620411092e458a47
rzmsv <rmussavi@gmail.com>
2024-12-24 11:29:35 +0330

    merge

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ function Section({children, title}: SectionProps): React.JSX.Element {
             color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
-        Hello WOOOOOOOORLD
+        {title}
       </Text>
       <Text
         style={[
