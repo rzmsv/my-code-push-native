2f0c89338b686fa3084ca3464d10a8e77004dc66
rzmsv <rmussavi@gmail.com>
2024-12-24 11:56:28 +0330

    change text

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ function Section({children, title}: SectionProps): React.JSX.Element {
             color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
-        {title}
+        HEllo WORLD
       </Text>
       <Text
         style={[
